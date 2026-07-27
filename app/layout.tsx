ff64d20e847bdf7342940cd5ed5295bd66cdaaa3
jawfbs <jake@fbsdata.com>
2026-07-27 13:57:20 -0500

    Load AI workspace styles globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./styles.css";
 import "./enhancements.css";
 import "./redesign.css";
+import "./workspace.css";
 import "./customize.css";
 
 export const metadata: Metadata = {
